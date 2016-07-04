e84271dd95945645c8ca2c649ea660686909dbac
winlin <winlin@vip.126.com>
2016-07-04 11:49:43 +0800

    resample sample rate.

--- aresample/aresample_test.go
+++ aresample/aresample_test.go
@@ -23,34 +23,478 @@ package aresample
 
 import (
 	"testing"
+	"bytes"
 )
 
 func TestPcmS16leMono2Stereo(t *testing.T) {
 	if err := PcmS16leMono2Stereo(make([]byte, 1), make([]byte, 2)); err == nil {
-		t.Error("invalid pcm")
-		return
+		t.Error("invalid pcm, err is", err)
 	}
 
 	if err := PcmS16leMono2Stereo(make([]byte, 1), make([]byte, 3)); err == nil {
-		t.Error("invalid pcm")
-		return
+		t.Error("invalid pcm, err is", err)
 	}
 
 	if err := PcmS16leMono2Stereo(make([]byte, 0), nil); err == nil {
-		t.Error("invalid pcm")
-		return
+		t.Error("invalid pcm, err is", err)
 	}
 
 	b := []byte{0x01, 0x02}
 	b0 := make([]byte, len(b) * 2)
 	if err := PcmS16leMono2Stereo(b, b0); err != nil {
 		t.Error("resample failed, err is", err)
-		return
 	} else if len(b0) != 2*len(b) {
 		t.Error("invalid resample", len(b0))
-		return
-	} else if b[0] == b0[0] || b[1] == b0[1] || b[0] == b0[2] || b[1] == b0[3] {
+	} else if bytes.Compare(b0[:2], b0[2:]) != 0 {
 		t.Error("invalid resample", b0)
-		return
+	} else if bytes.Compare(b0[:2], b) == 0 || bytes.Compare(b0[2:], b) == 0 {
+		t.Error("invalid resample", b0)
 	}
+}
+
+func TestPcmS16leResample_Basic(t *testing.T) {
+	if _,err := NewPcmS16leResampler(0, 0, 0); err == nil {
+		t.Error("invalid resampler")
+	}
+	if _,err := NewPcmS16leResampler(3, 0, 0); err == nil {
+		t.Error("invalid resampler")
+	}
+	if _,err := NewPcmS16leResampler(1, 0, 0); err == nil {
+		t.Error("invalid resampler")
+	}
+	if _,err := NewPcmS16leResampler(1, 44100, 0); err == nil {
+		t.Error("invalid resampler")
+	}
+	if _,err := NewPcmS16leResampler(1, 0, 44100); err == nil {
+		t.Error("invalid resampler")
+	}
+
+	pfn0 := func(pcm []byte, f func(pcm, npcm []byte, err error)) {
+		if v,err := NewPcmS16leResampler(1, 44100, 44100); err != nil {
+			t.Error("invalid resampler")
+		} else {
+			npcm,err := v.Resample(pcm)
+			f(pcm, npcm, err)
+		}
+	}
+	pfn0(nil, func(pcm,npcm []byte, err error){
+		if err == nil {
+			t.Error("invalid pcm, err is", err)
+		}
+	})
+	pfn0([]byte{}, func(pcm,npcm []byte, err error){
+		if err == nil {
+			t.Error("invalid pcm, err is", err)
+		}
+	})
+	pfn0([]byte{0x00}, func(pcm,npcm []byte, err error){
+		if err == nil {
+			t.Error("invalid pcm, err is", err)
+		}
+	})
+
+	pfn1 := func(pcm []byte, f func(pcm, npcm []byte, err error)) {
+		if v,err := NewPcmS16leResampler(2, 44100, 44100); err != nil {
+			t.Error("invalid resampler")
+		} else {
+			npcm,err := v.Resample(pcm)
+			f(pcm, npcm, err)
+		}
+	}
+	pfn1(nil, func(pcm,npcm []byte, err error){
+		if err == nil {
+			t.Error("invalid pcm, err is", err)
+		}
+	})
+	pfn1([]byte{}, func(pcm,npcm []byte, err error){
+		if err == nil {
+			t.Error("invalid pcm, err is", err)
+		}
+	})
+	pfn1([]byte{0x00}, func(pcm,npcm []byte, err error){
+		if err == nil {
+			t.Error("invalid pcm, err is", err)
+		}
+	})
+	pfn1([]byte{0x00,0x01}, func(pcm,npcm []byte, err error){
+		if err == nil {
+			t.Error("invalid pcm, err is", err)
+		}
+	})
+	pfn1([]byte{0x00,0x01,0x02}, func(pcm,npcm []byte, err error){
+		if err == nil {
+			t.Error("invalid pcm, err is", err)
+		}
+	})
+}
+
+func TestPcmS16leResample_Mono(t *testing.T) {
+	pfn0 := func(pcm []byte, f func(pcm, npcm []byte, err error)) {
+		if v,err := NewPcmS16leResampler(1, 44100, 44100); err != nil {
+			t.Error("invalid resampler")
+		} else {
+			npcm,err := v.Resample(pcm)
+			f(pcm, npcm, err)
+		}
+	}
+	pfn0([]byte{0x00,0x00}, func(pcm,npcm []byte, err error){
+		if err != nil {
+			t.Error("invalid pcm, err is", err)
+		}
+		if len(npcm) != 2 {
+			t.Error("invalid pcm", npcm)
+		}
+	})
+	pfn0([]byte{0x00,0x01, 0x02,0x03}, func(pcm,npcm []byte, err error){
+		if err != nil {
+			t.Error("invalid pcm, err is", err)
+		}
+		if len(npcm) != 4 {
+			t.Error("invalid pcm", npcm)
+		}
+	})
+
+	pfn1 := func(pcm []byte, f func(pcm,npcm []byte, err error)){
+		if v,err := NewPcmS16leResampler(1, 44100, 22050); err != nil {
+			t.Error("invalid pcm, err is", err)
+		} else {
+			npcm,err := v.Resample(pcm)
+			f(pcm, npcm, err)
+		}
+	}
+	pfn1([]byte{0x00,0x01, 0x02,0x03}, func(pcm,npcm []byte, err error){
+		if err != nil {
+			t.Error("invalid pcm, err is", err)
+		}
+		if len(npcm) != 2 {
+			t.Error("invalid pcm", npcm)
+		}
+	})
+	pfn1([]byte{0x00,0x01, 0x02,0x03, 0x04,0x05, 0x06,0x07}, func(pcm,npcm []byte, err error){
+		if err != nil {
+			t.Error("invalid pcm, err is", err)
+		}
+		if len(npcm) != 4 {
+			t.Error("invalid pcm", npcm)
+		}
+	})
+
+	pfn2 := func(pcm []byte, f func(pcm,npcm []byte, err error)){
+		if v,err := NewPcmS16leResampler(1, 22050, 44100); err != nil {
+			t.Error("invalid pcm, err is", err)
+		} else {
+			npcm,err := v.Resample(pcm)
+			f(pcm, npcm, err)
+		}
+	}
+	pfn2([]byte{0x00,0x01}, func(pcm,npcm []byte, err error){
+		if err != nil {
+			t.Error("invalid pcm, err is", err)
+		}
+		if len(npcm) != 4 {
+			t.Error("invalid pcm", npcm)
+		}
+	})
+	pfn2([]byte{0x00,0x01, 0x02,0x03,}, func(pcm,npcm []byte, err error){
+		if err != nil {
+			t.Error("invalid pcm, err is", err)
+		}
+		if len(npcm) != 8 {
+			t.Error("invalid pcm", npcm)
+		}
+	})
+
+	pfn3 := func(pcm []byte, f func(pcm,npcm []byte, err error)){
+		if v,err := NewPcmS16leResampler(1, 22050, 32000); err != nil {
+			t.Error("invalid pcm, err is", err)
+		} else {
+			npcm,err := v.Resample(pcm)
+			f(pcm, npcm, err)
+		}
+	}
+	pfn3([]byte{0x00,0x01}, func(pcm,npcm []byte, err error){
+		if err != nil {
+			t.Error("invalid pcm, err is", err)
+		}
+		if len(npcm) != 2 {
+			t.Error("invalid pcm", npcm)
+		}
+	})
+	pfn3([]byte{0x00,0x01, 0x02,0x03}, func(pcm,npcm []byte, err error){
+		if err != nil {
+			t.Error("invalid pcm, err is", err)
+		}
+		if len(npcm) != 4 {
+			t.Error("invalid pcm", npcm)
+		}
+	})
+	pfn3([]byte{0x00,0x01, 0x02,0x03, 0x04,0x05}, func(pcm,npcm []byte, err error){
+		if err != nil {
+			t.Error("invalid pcm, err is", err)
+		}
+		if len(npcm) != 8 {
+			t.Error("invalid pcm", npcm)
+		}
+	})
+
+	pfn4 := func(pcm []byte, f func(pcm,npcm []byte, err error)){
+		if v,err := NewPcmS16leResampler(1, 32000, 22050); err != nil {
+			t.Error("invalid pcm, err is", err)
+		} else {
+			npcm,err := v.Resample(pcm)
+			f(pcm, npcm, err)
+		}
+	}
+	pfn4([]byte{0x00,0x01}, func(pcm,npcm []byte, err error){
+		if err != nil {
+			t.Error("invalid pcm, err is", err)
+		}
+		if len(npcm) != 0 {
+			t.Error("invalid pcm", npcm)
+		}
+	})
+	pfn4([]byte{0x00,0x01, 0x02,0x03}, func(pcm,npcm []byte, err error){
+		if err != nil {
+			t.Error("invalid pcm, err is", err)
+		}
+		if len(npcm) != 2 {
+			t.Error("invalid pcm", npcm)
+		}
+	})
+	pfn4([]byte{0x00,0x01, 0x02,0x03, 0x04,0x05}, func(pcm,npcm []byte, err error){
+		if err != nil {
+			t.Error("invalid pcm, err is", err)
+		}
+		if len(npcm) != 4 {
+			t.Error("invalid pcm", npcm)
+		}
+	})
+	pfn4([]byte{0x00,0x01, 0x02,0x03, 0x04,0x05, 0x06,0x07}, func(pcm,npcm []byte, err error){
+		if err != nil {
+			t.Error("invalid pcm, err is", err)
+		}
+		if len(npcm) != 4 {
+			t.Error("invalid pcm", npcm)
+		}
+	})
+}
+
+func TestPcmS16leResample_MonoSamples(t *testing.T) {
+	var err error
+	var v ResampleSampleRate
+	if v,err = NewPcmS16leResampler(1, 44100, 22050); err != nil {
+		t.Error("invalid resampler")
+	}
+
+	var npcm []byte
+	if npcm,err = v.Resample([]byte{0x00,0x01}); err != nil {
+		t.Error("invalid pcm, err is", err)
+	}
+	if len(npcm) != 0 {
+		t.Error("invalid pcm", npcm)
+	}
+
+	if npcm,err = v.Resample([]byte{0x00,0x02}); err != nil {
+		t.Error("invalid pcm, err is", err)
+	}
+	if len(npcm) != 2 {
+		t.Error("invalid pcm", npcm)
+	}
+
+	if npcm,err = v.Resample([]byte{0x00,0x02, 0x03,0x04}); err != nil {
+		t.Error("invalid pcm, err is", err)
+	}
+	if len(npcm) != 2 {
+		t.Error("invalid pcm", npcm)
+	}
+
+	if npcm,err = v.Resample([]byte{0x00,0x02}); err != nil {
+		t.Error("invalid pcm, err is", err)
+	}
+	if len(npcm) != 0 {
+		t.Error("invalid pcm", npcm)
+	}
+
+	//////////////////////////////////////////////////////////////////////
+	if v,err = NewPcmS16leResampler(1, 22050, 32000); err != nil {
+		t.Error("invalid resampler")
+	}
+	if npcm,err = v.Resample([]byte{0x00,0x01}); err != nil {
+		t.Error("invalid pcm, err is", err)
+	}
+	if len(npcm) != 2 {
+		t.Error("invalid pcm", npcm)
+	}
+
+	if npcm,err = v.Resample([]byte{0x00,0x02}); err != nil {
+		t.Error("invalid pcm, err is", err)
+	}
+	if len(npcm) != 2 {
+		t.Error("invalid pcm", npcm)
+	}
+
+	if npcm,err = v.Resample([]byte{0x00,0x03}); err != nil {
+		t.Error("invalid pcm, err is", err)
+	}
+	if len(npcm) != 4 {
+		t.Error("invalid pcm", npcm)
+	}
+
+	if npcm,err = v.Resample([]byte{0x00,0x04}); err != nil {
+		t.Error("invalid pcm, err is", err)
+	}
+	if len(npcm) != 2 {
+		t.Error("invalid pcm", npcm)
+	}
+}
+
+func TestPcmS16leResample_Stereo(t *testing.T) {
+	pfn0 := func(pcm []byte, f func(pcm, npcm []byte, err error)) {
+		if v,err := NewPcmS16leResampler(2, 44100, 44100); err != nil {
+			t.Error("invalid resampler")
+		} else {
+			npcm,err := v.Resample(pcm)
+			f(pcm, npcm, err)
+		}
+	}
+	pfn0([]byte{0x00,0x00,0x00,0x00}, func(pcm,npcm []byte, err error){
+		if err != nil {
+			t.Error("invalid pcm, err is", err)
+		}
+		if len(npcm) != 2*2 {
+			t.Error("invalid pcm", npcm)
+		}
+	})
+	pfn0([]byte{0x00,0x01,0x00,0x01, 0x02,0x03,0x02,0x03}, func(pcm,npcm []byte, err error){
+		if err != nil {
+			t.Error("invalid pcm, err is", err)
+		}
+		if len(npcm) != 4*2 {
+			t.Error("invalid pcm", npcm)
+		}
+	})
+
+	pfn1 := func(pcm []byte, f func(pcm,npcm []byte, err error)){
+		if v,err := NewPcmS16leResampler(2, 44100, 22050); err != nil {
+			t.Error("invalid pcm, err is", err)
+		} else {
+			npcm,err := v.Resample(pcm)
+			f(pcm, npcm, err)
+		}
+	}
+	pfn1([]byte{0x00,0x01,0x00,0x01, 0x02,0x03,0x02,0x03,}, func(pcm,npcm []byte, err error){
+		if err != nil {
+			t.Error("invalid pcm, err is", err)
+		}
+		if len(npcm) != 2*2 {
+			t.Error("invalid pcm", npcm)
+		}
+	})
+	pfn1([]byte{0x00,0x01,0x00,0x01, 0x02,0x03,0x02,0x03, 0x04,0x05,0x04,0x05, 0x06,0x07,0x06,0x07,}, func(pcm,npcm []byte, err error){
+		if err != nil {
+			t.Error("invalid pcm, err is", err)
+		}
+		if len(npcm) != 4*2 {
+			t.Error("invalid pcm", npcm)
+		}
+	})
+
+	pfn2 := func(pcm []byte, f func(pcm,npcm []byte, err error)){
+		if v,err := NewPcmS16leResampler(2, 22050, 44100); err != nil {
+			t.Error("invalid pcm, err is", err)
+		} else {
+			npcm,err := v.Resample(pcm)
+			f(pcm, npcm, err)
+		}
+	}
+	pfn2([]byte{0x00,0x01,0x00,0x01,}, func(pcm,npcm []byte, err error){
+		if err != nil {
+			t.Error("invalid pcm, err is", err)
+		}
+		if len(npcm) != 4*2 {
+			t.Error("invalid pcm", npcm)
+		}
+	})
+	pfn2([]byte{0x00,0x01,0x00,0x01, 0x02,0x03,0x02,0x03,}, func(pcm,npcm []byte, err error){
+		if err != nil {
+			t.Error("invalid pcm, err is", err)
+		}
+		if len(npcm) != 8*2 {
+			t.Error("invalid pcm", npcm)
+		}
+	})
+
+	pfn3 := func(pcm []byte, f func(pcm,npcm []byte, err error)){
+		if v,err := NewPcmS16leResampler(2, 22050, 32000); err != nil {
+			t.Error("invalid pcm, err is", err)
+		} else {
+			npcm,err := v.Resample(pcm)
+			f(pcm, npcm, err)
+		}
+	}
+	pfn3([]byte{0x00,0x01,0x00,0x01,}, func(pcm,npcm []byte, err error){
+		if err != nil {
+			t.Error("invalid pcm, err is", err)
+		}
+		if len(npcm) != 2*2 {
+			t.Error("invalid pcm", npcm)
+		}
+	})
+	pfn3([]byte{0x00,0x01,0x00,0x01, 0x02,0x03,0x02,0x03,}, func(pcm,npcm []byte, err error){
+		if err != nil {
+			t.Error("invalid pcm, err is", err)
+		}
+		if len(npcm) != 4*2 {
+			t.Error("invalid pcm", npcm)
+		}
+	})
+	pfn3([]byte{0x00,0x01,0x00,0x01, 0x02,0x03,0x02,0x03, 0x04,0x05,0x04,0x05,}, func(pcm,npcm []byte, err error){
+		if err != nil {
+			t.Error("invalid pcm, err is", err)
+		}
+		if len(npcm) != 8*2 {
+			t.Error("invalid pcm", npcm)
+		}
+	})
+
+	pfn4 := func(pcm []byte, f func(pcm,npcm []byte, err error)){
+		if v,err := NewPcmS16leResampler(2, 32000, 22050); err != nil {
+			t.Error("invalid pcm, err is", err)
+		} else {
+			npcm,err := v.Resample(pcm)
+			f(pcm, npcm, err)
+		}
+	}
+	pfn4([]byte{0x00,0x01,0x00,0x01,}, func(pcm,npcm []byte, err error){
+		if err != nil {
+			t.Error("invalid pcm, err is", err)
+		}
+		if len(npcm) != 0*2 {
+			t.Error("invalid pcm", npcm)
+		}
+	})
+	pfn4([]byte{0x00,0x01,0x00,0x01, 0x02,0x03,0x02,0x03,}, func(pcm,npcm []byte, err error){
+		if err != nil {
+			t.Error("invalid pcm, err is", err)
+		}
+		if len(npcm) != 2*2 {
+			t.Error("invalid pcm", npcm)
+		}
+	})
+	pfn4([]byte{0x00,0x01,0x00,0x01, 0x02,0x03,0x02,0x03, 0x04,0x05,0x04,0x05,}, func(pcm,npcm []byte, err error){
+		if err != nil {
+			t.Error("invalid pcm, err is", err)
+		}
+		if len(npcm) != 4*2 {
+			t.Error("invalid pcm", npcm)
+		}
+	})
+	pfn4([]byte{0x00,0x01,0x00,0x01, 0x02,0x03,0x02,0x03, 0x04,0x05,0x04,0x05, 0x06,0x07,0x06,0x07,}, func(pcm,npcm []byte, err error){
+		if err != nil {
+			t.Error("invalid pcm, err is", err)
+		}
+		if len(npcm) != 4*2 {
+			t.Error("invalid pcm", npcm)
+		}
+	})
 }
---
+++ aresample/samplerate.go
@@ -0,0 +1,128 @@
+// The MIT License (MIT)
+//
+// Copyright (c) 2016 winlin
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of
+// this software and associated documentation files (the "Software"), to deal in
+// the Software without restriction, including without limitation the rights to
+// use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+// the Software, and to permit persons to whom the Software is furnished to do so,
+// subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+// FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+// COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+// IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+// CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+// The PCM resample.
+package aresample
+
+import (
+	"fmt"
+)
+
+type ResampleSampleRate interface {
+	// Resample the pcm to npcm, which contains len(pcm)/2 samples.
+	// @remark each sample is 16bits in short int.
+	Resample(pcm []byte) (npcm []byte, err error)
+}
+
+// sample rate resampler.
+type srResampler struct {
+	channels        int    // Channels, L or LR
+	sampleRate      int    // Transform from this sample rate.
+	nSampleRate     int    // Transform to this sample rate.
+
+	nbSamples       uint64 // Total samples we got from input.
+	nbOutputSamples uint64 // Total samples we put to output.
+}
+
+// Create resampler to transform pcm
+// from sampleRate to nSampleRate, where pcm contains number of channels
+// @remark each sample is 16bits in short int.
+func NewPcmS16leResampler(channels, sampleRate int, nSampleRate int) (ResampleSampleRate, error) {
+	if channels < 1 || channels > 2 {
+		return nil,fmt.Errorf("invalid channels=%v", channels)
+	}
+	if sampleRate <= 0 {
+		return nil,fmt.Errorf("invalid sampleRate=%v", sampleRate)
+	}
+	if nSampleRate <= 0 {
+		return nil,fmt.Errorf("invalid nSampleRate=%v", nSampleRate)
+	}
+
+	v := &srResampler{
+		channels: channels,
+		sampleRate: sampleRate,
+		nSampleRate: nSampleRate,
+	}
+
+	return v,nil
+}
+
+func (v *srResampler) Resample(pcm []byte) (npcm []byte, err error) {
+	if len(pcm) == 0 {
+		return nil,fmt.Errorf("empty pcm")
+	}
+	if (len(pcm)%(2*v.channels)) != 0 {
+		return nil,fmt.Errorf("invalid pcm, should mod(%v)", 2*v.channels)
+	}
+
+	if v.sampleRate == v.nSampleRate {
+		return pcm[:],nil
+	}
+
+	// Convert pcm to int16 values
+	ipcm := []int16{}
+
+	for i:=0; i<len(pcm); i+=2 {
+		// 16bits le sample
+		v := (int16(pcm[i])) | (int16(pcm[i+1]) << 8)
+
+		ipcm = append(ipcm, v)
+	}
+
+	// Resample the ipcm
+	opcm := []int16{}
+	for i:=0; i<len(ipcm); i+=v.channels {
+		// How many samples should we output
+		v.nbSamples++
+		outputSamples := uint64(float64(v.nbSamples) * float64(v.nSampleRate) / float64(v.sampleRate))
+
+		// Drop samples
+		if outputSamples <= v.nbOutputSamples {
+			continue
+		}
+
+		// Normally insert current sample.
+		if outputSamples == v.nbOutputSamples + 1 {
+			for j:=0; j<v.channels; j++ {
+				opcm = append(opcm, ipcm[i + j])
+			}
+			v.nbOutputSamples++
+			continue
+		}
+
+		// Duplicate current sample N times.
+		for k:=v.nbOutputSamples; k < outputSamples; k++ {
+			for j:=0; j<v.channels; j++ {
+				opcm = append(opcm, ipcm[i + j])
+			}
+			v.nbOutputSamples++
+		}
+	}
+
+	// Convert int16 samples to bytes.
+	npcm = []byte{}
+	for _,v := range opcm {
+		npcm = append(npcm, byte(v))
+		npcm = append(npcm, byte(v >> 8))
+	}
+
+	return
+}
